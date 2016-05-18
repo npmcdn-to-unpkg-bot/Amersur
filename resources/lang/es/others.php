9a9c8b6618c2e08b31c1b27a9d5785e1acaa0ec6
Marco Lopez <mlopez@marostdevelopers.com>
2016-05-17 21:57:50 -0500

    lang es

---
+++ resources/lang/es/others.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+	"create"		=> "Creado",
+	"update"       	=> "Actualizado",
+	"delete"       	=> "Eliminado",
+	"restore"      	=> "Restaurado",
+	"admin"		   	=> "Administrador",
+	"editor"		=> "Editor",
+	"reportero"		=> "Reportero"
+
+);
