c7f512479c4612e05ad651ff36bf4790211a962a
Marco Lopez <mlopez@marostdevelopers.com>
2016-05-25 19:55:21 -0500

    diseño de email de form contacto

---
+++ resources/views/emails/frontend/contacto.blade.php
@@ -0,0 +1,95 @@
+<html>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+<table style="padding:30px 10px;background:#F4F4F4;width:100%;font-family:arial" cellpadding="0" cellspacing="0">
+
+    <tbody>
+    <tr>
+        <td>
+
+            <table style="max-width:540px;min-width:320px" align="center" cellspacing="0">
+                <tbody>
+
+                <tr>
+                    <td style="background:#fff;border:1px solid #D8D8D8;padding:30px 30px" align="center">
+
+                        <table align="center">
+                            <tbody>
+
+                            <tr>
+                                <td style="border-bottom:1px solid #D8D8D8;color:#666;text-align:center;padding-bottom:30px">
+
+                                    <table style="margin:auto" align="center">
+                                        <tbody>
+                                        <tr>
+                                            <td style="color:#005f84;font-size:22px;font-weight:bold;text-align:center;font-family:arial">
+                                                Contacto
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="color:#666;padding:15px; padding-bottom:0;font-size:14px;line-height:20px;font-family:arial;text-align:left">
+
+                                    <div style="font-style:normal;padding-bottom:15px;font-family:arial;line-height:20px;text-align:left">
+
+                                        <p><span style="font-weight:bold;font-size:16px">Nombre:</span> {{ $nombre }}</p>
+                                        <p><span style="font-weight:bold;font-size:16px">Email:</span> {{ $email }}</p>
+                                        <p><span style="font-weight:bold;font-size:16px">Mensaje:</span></p>
+                                        <p>{{ $mensaje }}</p>
+                                    </div>
+
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td style="background:#f9f9f9;border:1px solid #D8D8D8;border-top:none;padding:24px 10px" align="center">
+
+                        <table style="width:100%;max-width:650px" align="center">
+                            <tbody>
+
+                            <tr>
+                                <td style="font-size:20px;line-height:27px;text-align:center;max-width:650px">
+                                    <a href="#" style="text-decoration:none;color:#69696c" target="_blank">
+                                        <span style="color:#00ce00;font-weight:bold;max-width:180px"></span>
+                                    </a>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <table style="max-width:650px" align="center">
+                <tbody>
+                <tr>
+                    <td style="color:#b4b4b4;font-size:11px;padding-top:10px;line-height:15px;font-family:arial">
+                        <span> &copy; Todos los derechos reservados </span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+
+    </tbody>
+</table>
+</body>
+</html>
