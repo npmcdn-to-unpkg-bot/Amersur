dd68f2d4dda2d5b4dc0193dbe59533ada8b55993
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-04 16:33:44 -0500

    eliminacion de opciones a no utilizar

--- app/Entities/Admin/Distrito.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Amersur\Entities\Admin;
-
-use Amersur\Entities\BaseEntity;
-
-class Distrito extends BaseEntity{
-
-    protected $fillable = ['contenido_nosotros','contenido_pregunta','imagen','imagen_carpeta'];
-
-}
--- app/Entities/Admin/GalleryPhoto.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Amersur\Entities\Admin;
-
-use Amersur\Entities\BaseEntity;
-
-class GalleryPhoto extends BaseEntity{
-
-    protected $fillable = ['titulo'];
-
-} 
