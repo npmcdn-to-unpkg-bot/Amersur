fc913580e2a7d15876d49f11126f7eb3174dbaea
Marco Lopez <mlopez@marostdevelopers.com>
2016-07-04 10:47:53 -0500

    cargar datos de slider si titulo es diferente de vacio

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -20,6 +20,7 @@
                 {{-- IMAGEN --}}
                 <img src="{{ $slider_imagen }}" alt="" data-bgposition="center center" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
 
+                @if($slider_titulo <> "")
                 {{-- FONDO --}}
                 <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme" id="slide-18-layer-9" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['227','231','191','141']" data-width="['full','full','full','full']" data-height="['150','141','119','119']" data-whitespace="normal" data-transform_idle="o:1;" data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power4.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:0px;" data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000" data-responsive_offset="on" style="z-index: 5;background-color:rgba(255, 255, 255, 0.60);border-color:rgba(0, 0, 0, 0.50);"></div>
 
@@ -42,6 +43,7 @@
                 <div class="tp-caption tp-resizeme" id="slide-18-layer-14" data-x="['left','left','left','left']" data-hoffset="['38','27','27','27']" data-y="['top','top','top','top']" data-voffset="['518','516','516','516']" data-width="['570','634','634','634']" data-height="['76','77','77','77']" data-whitespace="normal" data-transform_idle="o:1;" data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:0px;s:inherit;e:inherit;" data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000" data-splitin="none" data-splitout="none" data-responsive_offset="on" style="z-index: 9; min-width: 570px; max-width: 570px; max-width: 76px; max-width: 76px; white-space: normal; font-size: 16px; line-height: 25px; font-weight: 400; color: rgba(12, 12, 12, 1.00);font-family:Arial, Helvetica, sans-serif;">
                     {{ $slider_descripcion }}
                 </div>
+                @endif
             </li>
             @endforeach
 
